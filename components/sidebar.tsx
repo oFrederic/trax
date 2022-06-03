8a0b6c6058decc665907879aed326f6a5e20125f
frederic wojcikowski <frederic.wojcikowski@tablecheck.com>
2022-06-03 22:26:23 +0900

    Update sidebar Component UI

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,8 @@
+import NextImage from "next/image";
+import NextLink from "next/link";
+
 import {
+  Flex,
   Box,
   List,
   ListItem,
@@ -16,8 +20,107 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
+const navMenu = [
+  { name: "Home", icon: MdHome, route: "/" },
+  { name: "Search", icon: MdSearch, route: "/search" },
+  { name: "Your Library", icon: MdLibraryMusic, route: "/library" },
+];
+
+const playlistMenu = [
+  { name: "Create Playlist", icon: MdPlaylistAdd, route: "/" },
+  { name: "Favorites", icon: MdFavorite, route: "/favorites" },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
+
 const Sidebar = () => {
-  return <div>SIDEBAR</div>;
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Flex flexDirection="column" paddingTop="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {playlistMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box paddingX="20px">
+          <Divider color="gray.800" />
+        </Box>
+        <Box
+          flex="1"
+          overflowY="auto"
+          paddingY="20px"
+          overflow="hidden"
+          _hover={{ overflowY: "scroll" }}
+          sx={{
+            "&::-webkit-scrollbar": {
+              backgroundColor: `black`,
+              width: "10px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: `gray`,
+              borderRadius: "8px",
+            },
+          }}
+        >
+          <List spacing={2}>
+            {playlists.map((song) => (
+              <ListItem paddingX="20px" key={song}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{song}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Sidebar;
